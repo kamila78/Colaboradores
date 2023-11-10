677192fc56add8ecda9116b778daa690d2107932
Kamila Alves de Carvalho Santos <91978705+kamila78@users.noreply.github.com>
2023-11-10 10:57:59 -0300

    Update_table_colaboradores

--- table_colaboradores.sql
+++ table_colaboradores.sql
@@ -1,7 +1,28 @@
-/*
--- Query: select count(*) from colaboradores where squad = 'farma'
-LIMIT 0, 200
+create database time_suporte;
+use time_suporte;
+create table colaboradores (
+id int not null primary key,
+nome varchar (255),
+data_nasc date,
+vencimento_ferias date,
+funcao varchar (255),
+nivel varchar (255),
+contratado date);
 
--- Date: 2023-11-10 10:50
-*/
-INSERT INTO `` (`count(*)`) VALUES (1);
+insert into colaboradores (nome, funcao, nivel, squad) values 
+( 'Kamila Alves', 'Supervisora', 'Senior', 'Full'),
+('Bruno Tributino', 'Analista', 'Junior', 'Corporate'),
+('Edmilson Santos', 'Estagiário', 'Estágio', 'Pro'),
+('Italo Rocha', 'Analista', 'Junior', 'Farma'),
+('João Victor', 'Analista', 'Pleno', 'Pro'),
+('Joselita Albuquerque', 'Analista', 'Pleno', 'Pro'),
+('Joyce Andrade', 'Analista', 'Pleno', 'Corporate'),
+('Luiza Barbosa', 'Estagiária', 'Estágio', 'Corporate');
+
+select * from colaboradores;
+/*Para visualizar as datas de aniversario no formato brasileiro*/
+select nome, date_format(data_nasc, '%d/%m/%y') as Aniversario from colaboradores; 
+/*para excluir uma coluna dentro de uma tabela*/
+alter table colaboradores drop column email;
+select count(*) from colaboradores where squad = 'farma';
+delete from colaboradores where nome is null
